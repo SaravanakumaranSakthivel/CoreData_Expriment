74a58124963fbef2509afd2a87d60b42c0690003
SaravanaKumaran Sakthivel <smartiphonedeveloper@gmail.com>
2021-04-17 08:36:59 -0400

    Added basic UI

--- CoreDataExpriment/CoreDataExpriment/ViewController.swift
+++ CoreDataExpriment/CoreDataExpriment/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var names: [String] = []
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        title = "The List"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
+    @IBAction func onClickAdd(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "New Name",
+                                      message: "Enter a New Name",
+                                      preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .default,
+                                       handler: { [unowned self] action in
+                                        
+                                        guard let textField = alert.textFields?.first,
+                                              let name = textField.text else {
+                                            return
+                                        }
+                                        self.names.append(name)
+                                        self.tableView.reloadData()
+                                        
+                                       })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+}
